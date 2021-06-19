0b6d2b948bd23c94a95e03607006305efbc765a0
supersloy <51485198+supersloy@users.noreply.github.com>
2021-06-19 19:36:48 +0300

    feat: Add dev server

--- builder/bin/cli.js
+++ builder/bin/cli.js
@@ -1,39 +1,48 @@
 #!/usr/bin/env node
-const { Command } = require('commander');
-const webpack = require('webpack');
+const { Command } = require("commander");
+const webpack = require("webpack");
+const webpackDevServer = require("webpack-dev-server");
+const paths = require("../scripts/paths");
+const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
+const openBrowser = require("react-dev-utils/openBrowser");
+
 const program = new Command();
-const chalk = require('react-dev-utils/chalk');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   throw err;
 });
 
 program
-  .option('-d, --dev', 'Start a development server')
-  .option('-b, --build', 'Build a production bundle')
-  .option('--public', 'Prefix for public assets', '')
-  .option('-m, --mode <type>', 'Mode (development / production)', 'development');
+  .option("-d, --dev", "Start a development server")
+  .option("-b, --build", "Build a production bundle")
+  .option("--public", "Prefix for public assets", "")
+  .option(
+    "-m, --mode <type>",
+    "Mode (development / production)",
+    "development"
+  );
 
 program.parse(process.argv);
 
 const options = program.opts();
 
-if (options.mode === 'development') {
-  process.env.NODE_ENV = 'development';
+if (options.mode !== "production") {
+  process.env.NODE_ENV = "development";
 } else {
-  process.env.NODE_ENV = 'production';
+  process.env.NODE_ENV = "production";
 }
 
+const configGenerator = require("../scripts/configGenerator");
 
-const configGenerator = require('../scripts/configGenerator');
-
-const config = configGenerator(process.env.NODE_ENV, options.public);
 if (options.build) {
-  console.log('Creating an optimized production build...');
+  console.log("Creating an optimized production build...");
+  const config = configGenerator(
+    process.env.NODE_ENV,
+    options.public || paths.output
+  );
 
   const compiler = webpack(config);
 
@@ -41,21 +50,36 @@ if (options.build) {
     compiler.run((err, stats) => {
       if (err) {
         if (!err.message) {
+          console.log("There is an error without a message.");
           return reject(err);
         }
 
         let errMessage = err.message;
 
-        reject(formatWebpackMessages({
-          errors: [errMessage],
-          warnings: [],
-        }));
+        reject(
+          formatWebpackMessages({
+            errors: [errMessage],
+            warnings: [],
+          })
+        );
       }
 
       return resolve();
     });
   })
-    .then(() => console.log('Compiled successfully'))
+    .then(() => console.log("Compiled successfully"))
     .catch((err) => console.error(err));
+}
+if (options.dev) {
+  console.log("Starting development server...");
 
+  const options = {
+    contentBase: paths.output,
+    hot: true,
+    host: "localhost",
+  };
+  const server = new webpackDevServer(compiler, options);
+  server.listen(5000, "localhost", () => {
+    console.log("dev server listening on port 5000");
+  });
 }
--- builder/scripts/configGenerator.js
+++ builder/scripts/configGenerator.js
@@ -1,73 +1,69 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const getClientEnvironment = require("./env");
 const paths = require("./paths");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const InterpolateHtmlPlugin = require('./InterpolateHtmlPlugin');
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const InterpolateHtmlPlugin = require("./InterpolateHtmlPlugin");
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
-module.exports = function (webpackEnv, publicPath = paths.output) {
-  const isEnvDevelopment = webpackEnv === 'development';
-  const isEnvProduction = webpackEnv === 'production';
+module.exports = function (webpackEnv, publicPath) {
+  const isEnvDevelopment = webpackEnv === "development";
+  const isEnvProduction = webpackEnv === "production";
 
   const env = getClientEnvironment(publicPath);
 
   return {
-    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    mode: isEnvProduction ? "production" : isEnvDevelopment && "development",
     // Stop compilation early in production
     bail: isEnvProduction,
-    devtool: isEnvDevelopment
-      ? 'source-map'
-      : false,
+    devtool: isEnvDevelopment ? "source-map" : false,
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
-    entry:
-      isEnvDevelopment
-        ? [
-          'react-hot-loader/patch',
-          paths.entryPoint,
-        ]
-        : [paths.entryPoint],
+    entry: isEnvDevelopment
+      ? ["react-hot-loader/patch", paths.entryPoint]
+      : [paths.entryPoint],
 
     output: {
       path: paths.output,
-      publicPath,
-      filename: isEnvDevelopment ? '[name].[fullhash].js' : '[name].[contenthash].js'
+      publicPath: publicPath || "auto",
+      filename: isEnvDevelopment
+        ? "[name].[fullhash].js"
+        : "[name].[contenthash].js",
     },
     module: {
       rules: [
         {
           test: /\.(js|mjs|jsx|ts|tsx)$/,
           exclude: /node_modules/,
-          loader: require.resolve('babel-loader'),
+          loader: require.resolve("babel-loader"),
           options: {
             presets: [
               [
-                '@babel/preset-env',
+                "@babel/preset-env",
                 {
                   targets: {
                     esmodules: true,
                   },
                 },
               ],
-              '@babel/preset-react',
-              '@babel/preset-typescript',
+              "@babel/preset-react",
+              "@babel/preset-typescript",
             ],
             plugins: [
               [
-                'babel-plugin-named-asset-import',
+                "babel-plugin-named-asset-import",
                 {
                   loaderMap: {
                     svg: {
-                      ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                      ReactComponent:
+                        "@svgr/webpack?-svgo,+titleProp,+ref![path]",
                     },
                   },
                 },
               ],
-              isEnvDevelopment && require.resolve('react-hot-loader/babel'),
+              isEnvDevelopment && require.resolve("react-hot-loader/babel"),
             ].filter(Boolean),
             // This is a feature of `babel-loader` for webpack (not Babel itself).
             // It enables caching results in ./node_modules/.cache/babel-loader/
@@ -81,88 +77,83 @@ module.exports = function (webpackEnv, publicPath = paths.output) {
         {
           test: /\.css$/,
           use: [
-            isEnvProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            isEnvProduction ? MiniCssExtractPlugin.loader : "style-loader",
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
-                importLoaders: 1
-              }
+                importLoaders: 1,
+              },
             },
-            'postcss-loader'
+            "postcss-loader",
           ].filter(Boolean),
-          exclude: /\.module\.css$/
+          exclude: /\.module\.css$/,
         },
         {
           test: /\.css$/,
           use: [
-            isEnvProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            isEnvProduction ? MiniCssExtractPlugin.loader : "style-loader",
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 importLoaders: 1,
-                modules: true
-              }
+                modules: true,
+              },
             },
-            'postcss-loader'
+            "postcss-loader",
           ].filter(Boolean),
-          include: /\.module\.css$/
+          include: /\.module\.css$/,
         },
         {
           test: /\.scss$/,
           use: [
-            isEnvProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            isEnvProduction ? MiniCssExtractPlugin.loader : "style-loader",
             //'style-loader',
-            'css-loader',
-            'sass-loader'
+            "css-loader",
+            "sass-loader",
           ].filter(Boolean),
-          exclude: /\.module\.css$/
+          exclude: /\.module\.css$/,
         },
         {
           test: /\.scss$/,
           use: [
-            isEnvProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            isEnvProduction ? MiniCssExtractPlugin.loader : "style-loader",
             //'style-loader',
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 importLoaders: 1,
-                modules: true
-              }
+                modules: true,
+              },
             },
-            'sass-loader'
+            "sass-loader",
           ].filter(Boolean),
-          include: /\.module\.css$/
+          include: /\.module\.css$/,
         },
         {
           test: /\.png$/,
           use: [
             {
-              loader: 'url-loader',
+              loader: "url-loader",
               options: {
-                mimetype: 'image/png'
-              }
-            }
-          ]
+                mimetype: "image/png",
+              },
+            },
+          ],
         },
         {
           test: /\.svg$/,
-          use: ['@svgr/webpack'],
+          use: ["@svgr/webpack"],
         },
-      ]
+      ],
     },
     resolve: {
-      extensions: [
-        '.js',
-        '.jsx',
-        '.tsx',
-        '.ts'
-      ],
+      extensions: [".js", ".jsx", ".tsx", ".ts"],
       alias: {
-        'react-dom': isEnvProduction ? 'react-dom' : '@hot-loader/react-dom'
-      }
+        "react-dom": isEnvProduction ? "react-dom" : "@hot-loader/react-dom",
+      },
     },
     devServer: {
-      contentBase: paths.output
+      contentBase: paths.output,
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
@@ -175,30 +166,30 @@ module.exports = function (webpackEnv, publicPath = paths.output) {
           },
           isEnvProduction
             ? {
-              minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true,
-              },
-            }
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
             : undefined
         )
       ),
       new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
       new webpack.DefinePlugin(env.stringified),
       isEnvProduction && new MiniCssExtractPlugin(),
-      new CleanWebpackPlugin()
+      new CleanWebpackPlugin(),
     ].filter(Boolean),
     optimization: {
       minimize: isEnvProduction,
-      runtimeChunk: 'single',
+      runtimeChunk: "single",
       // Automatically split vendor and commons
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
@@ -206,11 +197,11 @@ module.exports = function (webpackEnv, publicPath = paths.output) {
         cacheGroups: {
           vendor: {
             test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
-        }
-      }
-    }
+            name: "vendors",
+            chunks: "all",
+          },
+        },
+      },
+    },
   };
 };
